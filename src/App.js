1c178470831106c1382a5bd63ea1485ec0769080
YWilliam1 <103035432+YWilliam1@users.noreply.github.com>
2024-11-26 14:57:49 +0100

    [UPDATED] Added CV management

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import './Style.css'; 
+
+import ListeCvVisible from './pages/ListeCvVisible';
+import Login from "./pages/Login";
+import Register from "./pages/Register"; 
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import DashboardLayout from "./pages/DashboardLayout";
+import DetailsCv from "./pages/DetailsCv";
+import CreateCv from "./pages/CreateCv";
+import UpdateCv from "./pages/UpdateCv";
+import MesCv from "./pages/MesCv";
+import Accueil from "./pages/Accueil";
+import MesReco from "./pages/MesReco";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ListeCvVisible />} />
+        <Route path="/cv/details/:id" element={<DetailsCv />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<ProtectedRoute><DashboardLayout /></ProtectedRoute>}>
+          <Route index element={<Accueil />} />
+          <Route path="mes-cv" element={<MesCv />} />
+          <Route path="create-cv" element={<CreateCv />} />
+          <Route path="cv/updateCv/:id" element={<UpdateCv />} />
+          <Route path="reco" element={<MesReco />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
